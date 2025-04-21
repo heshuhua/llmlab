4a5abc19c3352e32b4eadae19f87b9cbf1767e3e
heshuhua <heshuhua@gmail.com>
2025-04-21 09:33:06 +0800

    add 20250421

---
+++ gradiolab/gradioapp.py
@@ -0,0 +1,9 @@
+import gradio as gr
+
+with gr.Blocks() as demo:
+    gr.Markdown("## fancy")
+    
+
+
+demo.launch()
+    
---
+++ pythonlab/labdebug.py
@@ -0,0 +1,11 @@
+import os
+
+numbers = [2,3,6,8]
+
+def time_two(values):
+    total =0 
+    for num in numbers:
+        total += num*2
+    return total
+
+print(time_two(5))
