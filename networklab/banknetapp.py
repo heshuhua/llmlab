c26b98a09aaa9fdee8f42ea088685636fd929938
heshuhua <heshuhua@gmail.com>
2025-04-23 16:14:57 +0800

    add 20250421

---
+++ networklab/banknetapp.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-1b6cade0a5fd4f00a41ec076486905e9", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
