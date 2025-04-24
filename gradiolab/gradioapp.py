ce21c3057d37de4c390d34b5e4aebf0e7e26ab99
heshuhua <heshuhua@gmail.com>
2025-04-24 09:28:05 +0800

    add 2025042401

--- gradiolab/gradioapp.py
+++ gradiolab/gradioapp.py
@@ -1,7 +1,12 @@
 import gradio as gr
+from langlab import 
+
+def greet(name):
+    return "hello "+name +"!"
 
 with gr.Blocks() as demo:
     gr.Markdown("## fancy")
+    gr.Interface(fn=greet,inputs="text",outputs="text")
     
 
 
